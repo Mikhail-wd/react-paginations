6b2ee64865b7ab9f64fd046c6b036fbac5adb328
Михаил <Supalis913@yahoo.com>
2024-02-28 17:34:30 +0700

    fix

--- src/components/tableBody.jsx
+++ src/components/tableBody.jsx
@@ -5,7 +5,7 @@ function tableBody({ array, page, fetchHeader }) {
     const [idDataArray, setIdDataArray] = useState(null)
     useEffect(() => {
         async function fetchingData() {
-            await fetch("http://api.valantis.store:40000/",
+            await fetch("https://api.valantis.store:40000/",
                 {
                     method: "POST",
                     headers: fetchHeader,
